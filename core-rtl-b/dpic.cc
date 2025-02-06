ecf332d57b7ac046eaa1da382b0f9d8cc49728e0
CmdBlock <54425466+CmdBlockZQG@users.noreply.github.com>
2025-02-07 04:44:46 +0800

    fix: rtl通过dpic访问外设地址和宽度不正确

--- core-rtl-b/dpic.cc
+++ core-rtl-b/dpic.cc
@@ -2,16 +2,28 @@
 
 #include "common.h"
 #include "soc.h"
+#include <cassert>
 
-void pmem_read(const int raddr, int *rdata) {
-  const paddr_t addr = raddr & ~0x3u;
-  *reinterpret_cast<word_t *>(rdata) = paddr_read(addr, 4);
+void pmem_read(const int raddr, const int rsize, int *rdata) {
+  word_t &res = *reinterpret_cast<word_t *>(rdata);
+  word_t data;
+  switch (rsize) {
+    case 0: data = paddr_read(raddr, 1); break;
+    case 1: data = paddr_read(raddr & ~0x1u, 2); break;
+    case 2: data = paddr_read(raddr & ~0x3u, 4); break;
+    default: assert(0);
+  }
+  data <<= (raddr & 0x3u) * 8;
+  res = data;
 }
 
-void pmem_write(const int waddr, const int wdata, const char wmask) {
-  const paddr_t addr = waddr & ~0x3u;
-  if (wmask & 0b0001) paddr_write(addr + 0, 1, wdata >> 0);
-  if (wmask & 0b0010) paddr_write(addr + 1, 1, wdata >> 8);
-  if (wmask & 0b0100) paddr_write(addr + 2, 1, wdata >> 16);
-  if (wmask & 0b1000) paddr_write(addr + 3, 1, wdata >> 24);
+void pmem_write(const int waddr, const int wsize, const int wdata, const char wmask) {
+  const word_t data = wdata >> ((waddr & 0x3u) * 8);
+  const word_t mask = wmask >> (waddr & 0x3u);
+  switch (wsize) {
+    case 0: assert(mask == 0x1); paddr_write(waddr, 1, data); break;
+    case 1: assert(mask == 0x3); paddr_write(waddr, 2, data); break;
+    case 2: assert(mask == 0xf); paddr_write(waddr, 4, data); break;
+    default: assert(0);
+  }
 }
